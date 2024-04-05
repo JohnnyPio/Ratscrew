043fcc13ff782c1486029069bdf2b67b39bab0a3
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-04 22:03:22 -0400

    Basic deck + shuffle and dealing

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import deck
 
 my_deck = deck.Deck()
-print(my_deck.shuffle())
+print("Original Deck:")
+print(my_deck)
+my_deck.shuffle()
+print("Shuffled Deck:")
+print(my_deck)
+print("Dealing a card:")
+print(my_deck.deal())
