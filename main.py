eddb6ce9b3b2b720c5879c7680c427af263a8526
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-04 22:05:43 -0400

    Quitting time :)

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ print("Shuffled Deck:")
 print(my_deck)
 print("Dealing a card:")
 print(my_deck.deal())
+
+# TODO - Going to want to have one class - card_hand. That will need at least two instances constructed, one for my
+#  hand and one for the computer's hand.
+
+
+
