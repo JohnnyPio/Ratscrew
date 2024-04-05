1f0985c1c853d80fc96065404ca59f8213179f08
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-05 17:35:28 -0400

    TODO

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ list_of_players = [computer_player, player_1]
 deck.initial_full_deck_deal_to_all_players(my_deck, list_of_players)
 print(f"computer player is {computer_player}")
 print(f"player1 is {player_1}")
+
+# TODO Should probably make a new Game class that involves a flip method for each player. Likely will need to inherit from the player classes. Will also need a pile class as well.
