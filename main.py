3449cba33153e2f14d5c8350a1342bf13e145eb8
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-06 10:45:33 -0400

    pile creation

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ computer_player = player.Player("computer")
 player_1 = player.Player("player1")
 list_of_players = [computer_player, player_1]
 
-deck.initial_full_deck_deal_to_all_players(my_deck, list_of_players)
+my_deck.initial_full_deck_deal_to_all_players(list_of_players)
 print(f"computer player is {computer_player}")
 print(f"player1 is {player_1}")
 
+pile = deck.Deck().empty()
+print(f"the pile is {pile}")
+
 # TODO Should probably make a new Game class that involves a flip method for each player. Likely will need to inherit from the player classes. Will also need a pile class as well.
+
