006e6536f2f54500f8c850ecc8550a4bce38e748
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-06 11:37:49 -0400

    Return card value instead of true in bool for royals

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
+ace = "Ace"
+king = "King"
+queen = "Queen"
+jack = "Jack"
+
+
 # TODO add a listener for the user to flip when pressing spacebar
 def flip_card(player, pile):
     flipped_card = player.cards[-1]
@@ -6,17 +12,20 @@ def flip_card(player, pile):
     player.cards.pop(-1)
     if card_is_royal(flipped_card):
         print(f"royal alert - a {flipped_card[0]} has been played")
+        # TODO Need to return a enum or something if this returns a royal as to what royal it is. That enum could
+        #  also be used as the return value in card_is_royal instead of just "True. Maybe return the string
+        #   value instead of True?
 
 
 def card_is_royal(card):
     print(f"the card is a {card[0]}")
-    if card[0] == "Ace":
-        return True
-    elif card[0] == "King":
-        return True
-    elif card[0] == "Queen":
-        return True
-    elif card[0] == "Jack":
-        return True
+    if card[0] == ace:
+        return ace
+    elif card[0] == king:
+        return king
+    elif card[0] == queen:
+        return queen
+    elif card[0] == jack:
+        return jack
     else:
         return False
--- main.py
+++ main.py
@@ -13,7 +13,6 @@ players_hands = [computer_player, player_1]
 
 my_deck.initial_full_deck_deal_to_all_players(players_hands)
 
-# TODO Should probably make a new Game class that involves a flip method for each player. Likely will need to inherit from the player classes. Will also need a pile class as well.
 while len(computer_player.cards) > 0 or len(player_1.cards) > 0:
     game.flip_card(computer_player, pile)
     game.flip_card(player_1, pile)
