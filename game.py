d6fc7122e71618c6e24dfdbe9df996375c587dd1
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-12 17:33:17 -0400

    Get Players from X functions

--- game.py
+++ game.py
@@ -11,3 +11,21 @@ def card_is_royal(card):
         return True
     else:
         return False
+
+
+def get_current_player_from_index(current_player_index, all_players):
+    return all_players[current_player_index]
+
+
+def get_index_from_player(current_player, all_players):
+    return all_players.index(current_player)
+
+
+def get_next_player_from_current_player(current_player, all_players):
+    current_player_index = get_index_from_player(current_player, all_players)
+    return all_players[(current_player_index + 1) % len(all_players)]
+
+
+def get_player_before_current_player(current_player, all_players):
+    current_player_index = get_index_from_player(current_player, all_players)
+    return all_players[(current_player_index - 1) % len(all_players)]
