b0ca81e9f3d4f57ed1965ebdb9f8f26ae01c56ec
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-12 17:35:39 -0400

    First really needed refactor :)

--- player.py
+++ player.py
@@ -1,6 +1,17 @@
 import game
 
 
+def max_cards_to_flip(last_card):
+    if last_card[0] == "Ace":
+        return 4
+    elif last_card[0] == "King":
+        return 3
+    elif last_card[0] == "Queen":
+        return 2
+    elif last_card[0] == "Jack":
+        return 1
+
+
 class Player:
     def __init__(self, name):
         self.name = name
@@ -26,65 +37,31 @@ class Player:
         self.cards.pop(-1)
         return flipped_card
 
-    # TODO More refactoring needed. Need to have a way to know if player runs out of cards
-    def flip_for_royal(self, last_card, current_pile):
-        max_cards = 0
+    def can_complete_flipping_for_royals(self, last_card, current_pile):
+        max_cards = max_cards_to_flip(last_card)
         flipped_cards = []
-        if last_card[0] == "Ace":
-            max_cards = 4
-            for _ in range(max_cards):
-                if len(self.cards) > 0:
-                    flipped_card = self.flip_single_card(current_pile)
-                    flipped_cards.append(flipped_card)
-                    if game.card_is_royal(flipped_card):
-                        return True
-                else:
-                    print("out of cards")
-                    return False
-            for card in flipped_cards:
-                if not game.card_is_royal(card):
-                    print("no royals here")
-                    return False
-            # TODO Add handling for player running out of cards
-        elif last_card[0] == "King":
-            max_cards = 3
-            for _ in range(3):
-                flipped_card = self.flip_single_card(current_pile)
-                flipped_cards.append(flipped_card)
-                if game.card_is_royal(flipped_card):
-                    return True
-            for card in flipped_cards:
-                if not game.card_is_royal(card):
-                    print("no royals here")
-                    return False
-        elif last_card[0] == "Queen":
-            max_cards = 2
-            for _ in range(max_cards):
-                flipped_card = self.flip_single_card(current_pile)
-                flipped_cards.append(flipped_card)
-                if game.card_is_royal(flipped_card):
-                    return True
-            for card in flipped_cards:
-                if not game.card_is_royal(card):
-                    print("no royals here")
-                    return False
-        elif last_card[0] == "Jack":
-            max_cards = 1
-            for _ in range(max_cards):
-                flipped_card = self.flip_single_card(current_pile)
-                flipped_cards.append(flipped_card)
-                if game.card_is_royal(flipped_card):
-                    return True
-            for card in flipped_cards:
-                if not game.card_is_royal(card):
-                    print("no royals here")
-                    return False
+        for _ in range(max_cards):
+            if not self.cards:
+                print("out of cards")
+                return False
 
-    def player_gets_pile(self, current_pile):
-        self.cards.append(current_pile)
+            flipped_card = self.flip_single_card(current_pile)
+            flipped_cards.append(flipped_card)
 
-    def __str__(self):
-        hand_str = ""
-        for card in self.cards:
-            hand_str += str(card) + "\n"
-        return hand_str
+            if game.card_is_royal(flipped_card):
+                return True
+
+        if not any(game.card_is_royal(card) for card in flipped_cards):
+            print("no royals here")
+            return False
+
+
+def player_gets_pile(self, current_pile):
+    self.cards.append(current_pile)
+
+
+def __str__(self):
+    hand_str = ""
+    for card in self.cards:
+        hand_str += str(card) + "\n"
+    return hand_str
