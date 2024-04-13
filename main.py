86e0d0b5f1b84c111d9b05217d8e7df7ee07a432
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-13 11:47:26 -0400

    Refactors + Fixes

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ player_1 = player.Player("player1")
 players_hands: list[Player] = [computer_player, player_1]
 my_deck.initial_full_deck_deal_to_all_players(players_hands)
 
+# Initialize the flipping
 player_index = 0
 current_player = game.get_current_player_from_index(player_index, players_hands)
 current_player.flip_single_card(pile)
@@ -21,9 +22,7 @@ current_player.flip_single_card(pile)
 both_players_have_more_than_zero_cards = True
 while both_players_have_more_than_zero_cards:
     current_card = pile.cards[-1]
-    next_player = game.get_next_player_from_current_player(current_player, players_hands)
-    current_player = next_player
-    print(f"the current player is {current_player.name}")
+    current_player = game.get_next_player_from_current_player(current_player, players_hands)
 
     if not game.card_is_royal(current_card):
         current_player.flip_single_card(pile)
@@ -33,18 +32,10 @@ while both_players_have_more_than_zero_cards:
             player_before = game.get_player_before_current_player(current_player, players_hands)
             print(f"{player_before.name} wins the pile")
             player_before.add_cards(list(pile.cards))
-            pile.cards = []  # TODO why doesn't pile.empty() work here?
-            # TODO Probably can just increment the index here? isntead of duplicating lines 18 and 19. Maybe move them inside the while loop?
-            next_card = player_before.flip_single_card(pile)
-            current_player = next_player
+            pile.empty()
+            player_before.flip_single_card(pile)
+            current_player = game.get_next_player_from_current_player(current_player, players_hands)
 
-    # TODO Move these into a few methods
-    # Truth Value Testing instead of len() > 0
-    if not computer_player.cards:
+    if not game.all_players_have_cards(players_hands):
         both_players_have_more_than_zero_cards = False
-        print(f"computer_player loses")
-        break
-    if not player_1.cards:
-        both_players_have_more_than_zero_cards = False
-        print(f"player_1 wins")
         break
