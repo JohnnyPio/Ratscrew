0e15ffc7a026f8b27a1676d2b2f269d59b63346f
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-19 13:10:13 -0400

    Making a Game Class

--- game.py
+++ game.py
@@ -1,7 +1,17 @@
 import time
+from deck import Deck
 
 
 # TODO add a listener for the user to flip when pressing spacebar
+
+class Game:
+    def __init__(self, players):
+        self.players = players
+        self.pile = Deck()
+        self.pile.shuffle()
+        self.pile.initial_full_deck_deal_to_all_players(self.players)
+        self.pile.empty()
+
 
 def card_is_royal(card):
     if card[0] == "Ace":
@@ -56,3 +66,31 @@ def delay_between_card_flips():
     # Should change based on easy,med,hard
     delay = 2  # x second delay
     time.sleep(delay)
+
+
+def computer_slap_delay():
+    # Should change based on easy,med,hard
+    delay = 0.5  # x second delay
+    time.sleep(delay)
+
+
+def matching_top_cards(pile):
+    if len(pile.cards) >= 2 and pile.cards[-1][0] == pile.cards[-2][0]:
+        return True
+
+
+def matching_sandwich_cards(pile):
+    if len(pile.cards) >= 3 and pile.cards[-1][0] == pile.cards[-3][0]:
+        return True
+
+
+def is_slappable_event(pile):
+    if matching_sandwich_cards(pile) or matching_top_cards(pile):
+        return True
+
+
+def players_in_slap_event(players):
+    all_players_to_slap = [players]
+    names = [player.name for player in all_players_to_slap]
+    print(f"here's who slapped {names}")
+    return all_players_to_slap
