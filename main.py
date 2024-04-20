e99a3285b0eb625fa3ce026416662db0e4ce729f
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-20 11:41:57 -0400

    Still WIP but in a much better spot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import deck
 import player
 import game
 import keyboard
-import threading
+
+import slap
 
 players_hands = []
 computer_player = player.Player("computer")
@@ -13,41 +13,43 @@ players_hands.append(player_1)
 my_game = game.Game(players_hands)
 
 
-def on_enter(event):
-    global enter_pressed
-    enter_pressed = True
+# def on_enter(event):
+#     global enter_pressed
+#     enter_pressed = True
+#
+#
+# def check_for_enter():
+#     global enter_pressed
+#     if enter_pressed:
+#         print("Enter key pressed")
+#         my_game.slap()
+#         enter_pressed = False
+
+    # if check_for_enter():
+    #     current_player = game.get_next_player_from_current_player(current_player, players_hands)
 
 
-def check_for_enter():
-    global enter_pressed
-    if enter_pressed:
-        print("Enter key pressed")
-        game.slap(my_game.pile, players_hands)
-        enter_pressed = False
-
-
-enter_pressed = False
-keyboard.on_press_key("enter", on_enter)
+# enter_pressed = False
+# keyboard.on_press_key("enter", on_enter)
 
 both_players_have_more_than_zero_cards = True
 while both_players_have_more_than_zero_cards:
     current_card = my_game.pile.cards[-1]
     my_game.get_next_player_from_current_player()
-    # if check_for_enter():
-    #     current_player = game.get_next_player_from_current_player(current_player, players_hands)
 
-    if not my_game.top_card_is_royal():
+    if not game.card_is_royal(current_card):
         my_game.current_player.flip_single_card(my_game.pile)
     else:
         # Need a way for the time delay inside this loop and also the slap listener
-        if not my_game.current_player.can_complete_flipping_for_royals(current_card, my_game.pile):
+        # TODO Slappable event not shown in royal sitch
+        if not my_game.can_complete_flipping_for_royals():
             my_game.player_wins_the_pile()
 
     # TODO This works well except in a royal situation. Need to have a way to pull royal sitch up
-    if game.is_slappable_event(my_game.pile):
-        game.computer_slap_delay()
-        game.players_in_slap_event(computer_player)
+    if my_game.is_slappable_event():
+        this_slap = slap.Slap()
+        this_slap.add_player_to_slap_pile(computer_player, game.computer_slap_delay())
 
-    if not game.all_players_have_cards(players_hands):
+    if not my_game.all_players_have_cards():
         both_players_have_more_than_zero_cards = False
         break
