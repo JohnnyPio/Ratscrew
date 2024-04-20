7c57087392f048487eb6285cd86a218a9f68ce5b
JohnnyPio <john.s.piotrowski@gmail.com>
2024-04-20 11:39:21 -0400

    More slap work

--- slap.py
+++ slap.py
@@ -5,3 +5,5 @@ class Slap:
     def add_player_to_slap_pile(self, player, slap_time):
         new_slapping_player_tuple = (player, slap_time)
         self.slapping_players = new_slapping_player_tuple
+        print(f"{player.name} added to slap pile")
+
