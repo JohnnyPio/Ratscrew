f417ff5ffcbd88df4fb87834f35e37d592136edb
JohnnyPio <john.s.piotrowski@gmail.com>
2024-05-15 20:07:43 -0400

    Cleaned up main...somewhat

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ players_hands.append(player_1)
 difficulty = input("Set game difficulty (1 = Easy | 2 = Medium | 3 = Hard): ")
 
 my_game = game.Game(players_hands, difficulty)
-my_game.observe_for_slap_opportunity.add_observer(my_game.monitor_for_slap_opportunity)
-my_game.observe_for_human_slap.add_observer(my_game.monitor_for_human_slap)
-my_game.observe_for_slap_opportunity.add_observer(my_game.all_players_have_cards)
+# TODO End Game observer not working
+# TODO Monitor for human slap not working, how does observe for slap have two observers are work???
 my_game.initialize_game()
 my_game.run_the_game()
