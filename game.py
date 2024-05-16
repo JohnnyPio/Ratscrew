39d1a64bfd1adc7e814f0d829d9813165c0fff6c
JohnnyPio <john.s.piotrowski@gmail.com>
2024-05-15 20:17:20 -0400

    Fixed end_game observer, tiny bugs are a killer

--- game.py
+++ game.py
@@ -53,7 +53,9 @@ class Game:
         self.difficulty = difficulty
 
     def monitor_for_end_game(self):
-        if not self.all_players_have_cards():
+        if self.all_players_have_cards():
+            pass
+        else:
             exit()
 
     def monitor_for_slap_opportunity(self):
@@ -62,7 +64,6 @@ class Game:
             print("computer Slaps")
             self.observe_for_slap_opportunity.remove_observers()
             self.observe_for_slap_opportunity.add_observer(self.monitor_for_slap_opportunity)
-            # TODO Not sure why this is showing a warning now?? Check git
             self.player_wins_the_pile(self.get_sole_bot_player())
             self.run_the_game()
 
@@ -88,7 +89,7 @@ class Game:
 
     def all_players_have_cards(self):
         for player in self.players:
-            if not player.cards:
+            if player.get_number_of_cards() == 0:
                 print(f"{player.name} loses, game over")
                 self.stop_dealing()
                 exit()
@@ -128,7 +129,7 @@ class Game:
 
     def notify_all_observers(self):
         self.observe_for_slap_opportunity.notify_observers()
-        self.observe_for_human_slap.notify_observers()
+        # self.observe_for_human_slap.notify_observers()
         self.observe_for_end_game.notify_observers()
 
     def add_card_to_pile(self, flipped_card):
@@ -137,7 +138,7 @@ class Game:
     def initialize_observers(self):
         self.observe_for_slap_opportunity.add_observer(self.monitor_for_slap_opportunity)
         # self.observe_for_human_slap.add_observer(self.monitor_for_human_slap)
-        self.observe_for_slap_opportunity.add_observer(self.all_players_have_cards)
+        self.observe_for_end_game.add_observer(self.all_players_have_cards)
 
     def initialize_game(self):
         self.initialize_observers()
