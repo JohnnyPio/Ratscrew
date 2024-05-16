6fc58e8ce655473777730e96384fc280a0634779
JohnnyPio <john.s.piotrowski@gmail.com>
2024-05-15 20:09:02 -0400

    WIP - cleaning up observers. Separating into 3 observers

--- game.py
+++ game.py
@@ -45,11 +45,16 @@ class Game:
     def __init__(self, players, difficulty):
         self.players = players
         self.pile = Deck()
+        self.observe_for_end_game = Observer()
         self.observe_for_slap_opportunity = Observer()
         self.observe_for_human_slap = Observer()
         self.should_continue_dealing = True
         self.current_player = None
         self.difficulty = difficulty
+
+    def monitor_for_end_game(self):
+        if not self.all_players_have_cards():
+            exit()
 
     def monitor_for_slap_opportunity(self):
         if self.is_slappable_event():
@@ -115,16 +120,27 @@ class Game:
         print(f"{self.players[index].name} has {self.players[index].get_number_of_cards()}")
 
     def flip_add_to_pile_then_remove_and_delay(self):
+        self.notify_all_observers()
         first_card = self.current_player.flip_single_card()
-        self.observe_for_slap_opportunity.notify_observers()
         self.add_card_to_pile(first_card)
         self.current_player.remove_top_card_from_hand()
         self.delay_between_card_flips()
 
+    def notify_all_observers(self):
+        self.observe_for_slap_opportunity.notify_observers()
+        self.observe_for_human_slap.notify_observers()
+        self.observe_for_end_game.notify_observers()
+
     def add_card_to_pile(self, flipped_card):
         self.pile.add_card(flipped_card)
 
+    def initialize_observers(self):
+        self.observe_for_slap_opportunity.add_observer(self.monitor_for_slap_opportunity)
+        # self.observe_for_human_slap.add_observer(self.monitor_for_human_slap)
+        self.observe_for_slap_opportunity.add_observer(self.all_players_have_cards)
+
     def initialize_game(self):
+        self.initialize_observers()
         self.pile.shuffle()
         self.initial_full_deck_deal_to_all_players()
         self.pile.empty()
